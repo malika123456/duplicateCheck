cc2a5c94cae837d17484694803435a248cfaeba1
mbanga-vs <malika.banga@vitaminshoppe.com>
2019-06-09 21:04:38 +0530

    Updates

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+if (!Array.prototype.fill) {
+  Object.defineProperty(Array.prototype, 'fill', {
+    value: function(value) {
+
+      // Steps 1-2.
+      if (this == null) {
+        throw new TypeError('this is null or not defined');
+      }
+
+      var O = Object(this);
+
+      // Steps 3-5.
+      var len = O.length >>> 0;
+
+      // Steps 6-7.
+      var start = arguments[1];
+      var relativeStart = start >> 0;
+
+      // Step 8.
+      var k = relativeStart < 0 ?
+        Math.max(len + relativeStart, 0) :
+        Math.min(relativeStart, len);
+
+      // Steps 9-10.
+      var end = arguments[2];
+      var relativeEnd = end === undefined ?
+        len : end >> 0;
+
+      // Step 11.
+      var final = relativeEnd < 0 ?
+        Math.max(len + relativeEnd, 0) :
+        Math.min(relativeEnd, len);
+
+      // Step 12.
+      while (k < final) {
+        O[k] = value;
+        k++;
+      }
+
+      // Step 13.
+      return O;
+    }
+  });
+}
+
 (function () {
 	const NUMBERS = 'numberList';
 
@@ -108,15 +153,19 @@
 		}
 
 		getSavedNumbers() {
-			return JSON.parse(window.sessionStorage.getItem(NUMBERS));
+			return window.sessionStorage ? JSON.parse(window.sessionStorage.getItem(NUMBERS)) : [];
 		}
 
 		removeSavedNumbers() {
-			window.sessionStorage.removeItem(NUMBERS);
+			if (window.sessionStorage) {
+				window.sessionStorage.removeItem(NUMBERS);
+			}
 		}
 
 		setSavedNumbers(numbers) {
-			window.sessionStorage.setItem(NUMBERS, JSON.stringify(numbers));
+			if (window.sessionStorage) {
+				window.sessionStorage.setItem(NUMBERS, JSON.stringify(numbers));
+			}
 		}
 
 		displaySavedNumbers() {
